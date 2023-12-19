e57859947cba96fb886bae799656248f00999772
Sa Sajjad <sqsa93103@gmail.com>
2023-12-19 20:35:42 +0600

    Fuck cr --git-lfs
    Signed-off-by: Sa Sajjad <sqsa93103@gmail.com>

---
+++ upload_apps.sh
@@ -0,0 +1,26 @@
+#*********************************************************************************************************#
+function file_push() {
+    curl -F document=@$1 "https://api.telegram.org/bot$bot_api/sendDocument" \
+         -F chat_id="$tg_id" \
+         -F "disable_web_page_preview=true" \
+         -F "parse_mode=html" \
+         -F caption="$2"
+}
+#*********************************************************************************************************#
+cd ~/$rom_name
+cd out/target/product/$device/system/system_etc/priv-app
+ls
+cd SystemUI
+zip SystemUI.zip SystemUI.apk 
+file_push "SystemUI.zip" "System Ui"
+cd ..
+cd Settings
+zip Settings.zip Settings.apk
+file_push "Settings.zip" "Settings"
+# cd ..
+# cd ..
+# cd ..
+# cd framework
+# zip snx.zip framework-res.apk
+# bash <(curl -s $script/tg_uploader.sh)
+# bash <(curl -s $script/dev_uploader.sh) -f framework.jar
