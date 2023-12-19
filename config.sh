c3253519ea293384ab90f5429e0422e6de71fa7c
Sa Sajjad <sqsa93103@gmail.com>
2023-12-19 10:02:10 +0600

    add some extra prop
    Signed-off-by: Sa Sajjad <sqsa93103@gmail.com>

--- config.sh
+++ config.sh
@@ -2,7 +2,11 @@ repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/androi
 git clone git@github.com:Sa-Sajjad/manifest.git --depth 1 -b cr-11 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 source build/envsetup.sh
+lunch lineage_lavender-userdebug
 export TZ=Asia/Dhaka
 export SELINUX_IGNORE_NEVERALLOWS=true
+make api-stubs-docs || echo no problem
+make system-api-stubs-docs || echo no problem
+make test-api-stubs-docs || echo no problem
 brunch lineage_lavender-userdebug
 Snx-R
