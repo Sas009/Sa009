a4096ae2d8cb698bbe90b366c7730762c636afad
Sa Sajjad <sqsa93103@gmail.com>
2023-12-20 08:09:04 +0600

    fuk
    Signed-off-by: Sa Sajjad <sqsa93103@gmail.com>

--- config.sh
+++ config.sh
@@ -1,12 +1,14 @@
 repo init --depth=1 --git-lfs --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 11.0 -g default,-mips,-darwin,-notdefault
 git clone git@github.com:Sa-Sajjad/manifest.git --depth 1 -b cr-11 .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -j$(nproc --all) --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync
 source build/envsetup.sh
 lunch lineage_lavender-userdebug
 export TZ=Asia/Dhaka
 export SELINUX_IGNORE_NEVERALLOWS=true
-# make api-stubs-docs || echo no problem
-# make system-api-stubs-docs || echo no problem
-# make test-api-stubs-docs || echo no problem
+export CONFIG_STATE_NOTIFIER=y
+brunch lineage_lavender-userdebug
+make api-stubs-docs || echo no problem
+make system-api-stubs-docs || echo no problem
+make test-api-stubs-docs || echo no problem
 brunch lineage_lavender-userdebug
 Snx-R
