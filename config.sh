1a9e068122fb417cae39b45118a0553511af6533
Sa Sajjad <sqsa93103@gmail.com>
2023-12-21 20:20:15 +0600

    test cr
    Signed-off-by: Sa Sajjad <sqsa93103@gmail.com>

--- config.sh
+++ config.sh
@@ -1,8 +1,7 @@
 # repo init --depth=1 --git-lfs --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 11.0 -g default,-mips,-darwin,-notdefault
-repo init -u https://github.com/crdroidandroid/android.git -b 11.0 --git-lfs
+repo init --depth=1 -u https://github.com/crdroidandroid/android.git -b 11.0 --git-lfs
 git clone git@github.com:Sa-Sajjad/manifest.git --depth 1 -b cr-11 .repo/local_manifests
-# repo sync -j$(nproc --all) --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync
-repo sync
+repo sync -j$(nproc --all) --no-clone-bundle --no-tags --optimized-fetch --prune
 source build/envsetup.sh
 lunch lineage_lavender-userdebug
 export TZ=Asia/Dhaka
