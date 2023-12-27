c0122d46044ae202e662848c87d278646eaf0fac
Sa Sajjad <sqsa93103@gmail.com>
2023-12-27 16:59:50 +0600

    Nusa test

--- config.sh
+++ config.sh
@@ -1,14 +1,29 @@
-repo init --depth=1 --git-lfs --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 11.0 -g default,-mips,-darwin,-notdefault
-git clone git@github.com:Sa-Sajjad/manifest.git --depth 1 -b cr-11 .repo/local_manifests
+# add repo, dt, vt, kt etc.
+repo init --depth=1 --no-repo-verify -u https://Sa-Sajjad:$ght@github.com/S-A-build/android_manifest_nusa.git -b 10
+# replace with your manifest
+git clone git@github.com:Sa-Sajjad/manifest.git --depth 1 -b ten-ksu .repo/local_manifests #ten-se-permissive
+# sync script
 repo sync -j$(nproc --all) --no-clone-bundle --no-tags --optimized-fetch --prune
+
 source build/envsetup.sh
-lunch lineage_lavender-userdebug
+lunch nad_lavender-userdebug 
+
+# export extra module
 export TZ=Asia/Dhaka
-# export SELINUX_IGNORE_NEVERALLOWS=true
-export CONFIG_STATE_NOTIFIER=y
-brunch lineage_lavender-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
 make api-stubs-docs || echo no problem
 make system-api-stubs-docs || echo no problem
 make test-api-stubs-docs || echo no problem
-brunch lineage_lavender-userdebug
+# vanilla build
+mka nad
+
+# gapps build
+# export USE_GAPPS=true
+# mka nad
+
+# # only specific app build
+# export BOARD_USES_SYSTEMUI=true
+# mka SystemUI
+# bash <(curl -s $script/upload_apps.sh)
 Snx-R
+export GAPPS_BUILD=t
