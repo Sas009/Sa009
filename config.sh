d71acc58e464876d0f4dd4912e8d9e080ffad6ff
Sa Sajjad <sqsa93103@gmail.com>
2024-01-01 08:50:44 +0600

    br
    Signed-off-by: Sa Sajjad <sqsa93103@gmail.com>

--- config.sh
+++ config.sh
@@ -4,10 +4,8 @@ repo init --depth=1 --no-repo-verify -u https://Sa-Sajjad:$ght@github.com/S-A-bu
 git clone git@github.com:Sa-Sajjad/manifest.git --depth 1 -b ten-ksu .repo/local_manifests #ten-se-permissive
 # sync script
 repo sync -j$(nproc --all) --no-clone-bundle --no-tags --optimized-fetch --prune
-
 source build/envsetup.sh
 lunch nad_lavender-userdebug 
-
 # export extra module
 export TZ=Asia/Dhaka
 export SELINUX_IGNORE_NEVERALLOWS=true
@@ -16,14 +14,10 @@ make system-api-stubs-docs || echo no problem
 make test-api-stubs-docs || echo no problem
 # vanilla build
 mka nad
-
-# gapps build
-# export USE_GAPPS=true
-# mka nad
-
+export GAPPS_BUILD=false
+Snx-R
+# export GAPPS_BUILD=true
 # # only specific app build
 # export BOARD_USES_SYSTEMUI=true
 # mka SystemUI
 # bash <(curl -s $script/upload_apps.sh)
-Snx-R
-export GAPPS_BUILD=true
