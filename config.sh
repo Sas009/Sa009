000af6f1306f1672e8a385e353588bc3ed77ab91
Sa Sajjad <sqsa93103@gmail.com>
2024-03-01 07:03:12 +0600

    matrix test
    Signed-off-by: Sa Sajjad <sqsa93103@gmail.com>

--- config.sh
+++ config.sh
@@ -6,7 +6,7 @@ git clone git@github.com:Sa-Sajjad/manifest.git --depth 1 -b mtx-14 .repo/local_
 # sync script
 repo sync -j$(nproc --all) --no-clone-bundle --no-tags --optimized-fetch --prune
 source build/envsetup.sh
-lunch nad_lavender-userdebug
+lunch lineage_lavender-userdebug
 # export extra module
 export TZ=Asia/Dhaka
 export SELINUX_IGNORE_NEVERALLOWS=true
@@ -14,7 +14,7 @@ make api-stubs-docs || echo no problem
 make system-api-stubs-docs || echo no problem
 make test-api-stubs-docs || echo no problem
 # vanilla build
-mka nad
+m bacon
 export GAPPS_BUILD=true
 Snx-R
 # export GAPPS_BUILD=true
