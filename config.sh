078b70649baa8952d811cd5f19f18fc417223fdd
Sa Sajjad <sqsa93103@gmail.com>
2024-03-01 07:44:46 +0600

    matrix test
    Signed-off-by: Sa Sajjad <sqsa93103@gmail.com>

--- config.sh
+++ config.sh
@@ -1,6 +1,7 @@
 ///# add repo, dt, vt, kt etc.
 # repo init --depth=1 --no-repo-verify -u https://Sa-Sajjad:$ght@github.com/S-A-build/android_manifest_nusa.git -b 10
-repo init -u https://github.com/ProjectMatrixx/android.git -b 14.0 --git-lfs
+# repo init -u https://github.com/ProjectMatrixx/android.git -b 14.0 --git-lfs
+repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
 # replace with your manifest
 git clone git@github.com:Sa-Sajjad/manifest.git --depth 1 -b mtx-14 .repo/local_manifests #ten-se-permissive
 # sync script
@@ -15,6 +16,7 @@ make system-api-stubs-docs || echo no problem
 make test-api-stubs-docs || echo no problem
 # vanilla build
 m bacon
+cat out/error.log
 export GAPPS_BUILD=true
 Snx-R
 # export GAPPS_BUILD=true
